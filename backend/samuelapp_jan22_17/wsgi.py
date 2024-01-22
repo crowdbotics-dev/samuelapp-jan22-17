34ae25a4d63ba7efaa0776fd7ce050a6ac9a0f77
Crowdbotics <engineering@crowdbotics.com>
2024-01-22 14:11:49 +0000

    Initial commit

---
+++ backend/samuelapp_jan22_17/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for samuelapp_jan22_17 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "samuelapp_jan22_17.settings"
+)
+
+application = get_wsgi_application()
